e5d26dd79385171c224019fd0bd02ed88a218646
Anto Sebastian <51168158+Antosebastian@users.noreply.github.com>
2020-03-27 03:48:53 -0400

    Update main.c

--- keypad_integration/keypad_i/main.c
+++ keypad_integration/keypad_i/main.c
@@ -2,9 +2,55 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <signal.h>
+#include <time.h>
 
 #define C_S system("clear")
-#define T_D usleep(1000000)
+#define T_D sleep(1)
+#define path /sys/class/gpio/gpio28/
+
+static inline void blink_deny()
+{
+//    int gpio_status = 1;
+//    gpio_status = access("/sys/class/gpio/gpio68/value", F_OK );
+//
+//    if (gpio_status == -1) 
+//    {
+//        printf("GPIO_68 file doesn't exist.\n");
+//        exit(1);
+//    }
+    system("echo out > path + direction"); 
+    sleep(1);
+    for(int i = 0; i < 3; i++)
+    {
+         printf("LED -> ON\n");
+         system("echo 1 > path + value");
+         printf("LED -> OFF\n");
+         system("echo 0 > path + value");
+    }
+    return;
+}
+
+static inline void blink_grant()
+{
+//    int gpio_status = 1;
+//    gpio_status = access("/sys/class/gpio/gpio68/value", F_OK );
+//
+//    if (gpio_status == -1) 
+//    {
+//        printf("GPIO_68 file doesn't exist.\n");
+//        exit(1);
+//    }
+    system("echo out > path + direction"); 
+    T_D;
+    printf("LED -> ON\n");
+    system("echo 1 > path + value");
+    T_D;
+    printf("LED -> OFF\n");
+    system("echo 0 > path + value");
+
+    return;
+}
 
 void get_password(char* ppassword)
 {
@@ -34,7 +80,8 @@ void door_open()
 {
     //control to gpio etc. 
     printf("Door open\n");//during implementation only a beep sound
-    T_D;
+    blink_grant();
+//    T_D;
     C_S;
     return;
 }
@@ -53,6 +100,7 @@ int main(int argc, char **argv)
         else
         {   
             printf("Wrong password; please retry\n");//during implementation only continuous beep sound
+            blink_deny();
             T_D;
             C_S;
         }
